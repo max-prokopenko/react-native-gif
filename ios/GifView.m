1ce88e5557c734ef055c6d6333a7384b0fc70e29
Max Prokopenko <max.prokopenko@hotmail.com>
2021-09-10 13:07:17 +0300

    feat: decrease fade in animation duration

--- ios/GifView.m
+++ ios/GifView.m
@@ -12,6 +12,7 @@
       _imageView = [SDAnimatedImageView new];
               
       _imageView.sd_imageTransition = SDWebImageTransition.fadeTransition;
+      _imageView.sd_imageTransition.duration = 0.25;
       _imageView.shouldIncrementalLoad = NO;
   }
   return self;
